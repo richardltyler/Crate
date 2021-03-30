08f464bcdec2ac99e120baf3ca06ba7f2ebe6307
Richard Tyler <richardtyler2011@gmail.com>
2021-03-29 18:34:42 -0600

    Install proptypes

---
+++ code/web/src/modules/survey/Survey.js
@@ -0,0 +1,6 @@
+// Imports
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+// import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router-dom'
+
