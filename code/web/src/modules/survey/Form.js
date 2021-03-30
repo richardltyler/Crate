e4141e2cedcc8daf900f2b9462aa5dc1a0f98622
Allison Dietz <dietz.allison@outlook.com>
2021-03-30 15:59:46 -0600

    Create state in Survey component, set imports in Form

---
+++ code/web/src/modules/survey/Form.js
@@ -0,0 +1,9 @@
+// Imports
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+// UI Imports
+import { Grid, GridCell } from '../../ui/grid'
+import { H3, H4 } from '../../ui/typography'
+import { grey, grey2 } from '../../ui/common/colors'
+
