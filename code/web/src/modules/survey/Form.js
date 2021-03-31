ce90316ac9e926d2f340b9cb3ed5a5c42a3505db
Allison Dietz <dietz.allison@outlook.com>
2021-03-30 20:29:55 -0600

    Connect basic Form layout for Survey to display

--- code/web/src/modules/survey/Form.js
+++ code/web/src/modules/survey/Form.js
@@ -1,41 +1,52 @@
 // Imports
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
-import { grey, grey2 } from '../../ui/common/colors'
+import { grey, grey2, grey3 } from '../../ui/common/colors'
 import Button from '../../ui/button'
 import ImageTile from '../../ui/image/Tile'
+import { level1 } from '../../ui/common/shadows'
+
+// APP IMPORTS
+import { APP_URL } from '../../setup/config/env'
+
 
 const Form = (props) => {
 
   const [stateSample, setStateSample] = useState({})
 
   return (
+    <section>
 
-    <>
-    <H4 >{ props.data.categoryTitle }</H4>
-    <p style={{ marginTop: '1em', color: grey2 }}>Please select at least one option, then click `NEXT` to continue...</p>
-    
-    <Button type="button" theme="secondary" disabled={false}>
-    Next ➤
-    </Button>
+      <Grid >
+          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+          <H4 >{ props.data.categoryTitle }</H4>
 
-    <Grid>
-      <GridCell justifyCenter={true}>
-        <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
-      </GridCell>
-      <GridCell justifyCenter={true}>
-        <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
-      </GridCell>
-      <GridCell justifyCenter={true}>
-        <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
-      </GridCell>
-    </Grid>
-    </>
+            <p style={{ marginTop: '1em', color: grey3 }}>Please select at least one option, then click `NEXT` to continue...</p>
+          </GridCell>
+      </Grid>
 
+      {/* <H4 >{ props.data.categoryTitle }</H4>
+      <p style={{ marginTop: '1em', color: grey2 }}>Please select at least one option, then click `NEXT` to continue...</p> */}
+
+      <Grid>
+        <GridCell justifyCenter={true}>
+          <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
+        </GridCell>
+        <GridCell justifyCenter={true}>
+          <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
+        </GridCell>
+        <GridCell justifyCenter={true}>
+          <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
+        </GridCell>
+      </Grid>
+      <Button type="button" theme="secondary" disabled={false}>
+      Next ➤
+      </Button>
+    </section>
   )
 }
 
