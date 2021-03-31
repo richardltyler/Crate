c86c7474b35d83a120cbd9925d2f125c4febca16
ninesky00 <jiangzho@gmail.com>
2021-03-30 21:30:18 -0500

    add user query test

---
+++ code/api/src/modules/user/tests/userQuery.test.js
@@ -0,0 +1,42 @@
+import request from 'supertest'; // import request library from supertest
+import express from 'express'; // import express so we can create a mock server
+import graphqlHTTP from 'express-graphql'; // import graphqlHTTP express library
+import schema from '../../../setup/schema'; // import our graphql schema
+
+// we create describe functions similar to RSpec
+describe('user queries', async () => {
+  let server = express();
+
+  beforeAll(() => {
+    server.use(
+      "/",
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false
+      })
+    )
+  });
+
+  it('returns all users emails', async(done) => {
+    const query = `{
+      users {
+        email
+      }
+    }`;
+    const response = await request(server) 
+      .post('/')
+      .send({query: query})
+      .expect(200)
+
+      expect(response.body).toMatchObject({
+        data: {
+          users: [
+            {email: 'admin@crate.com'},
+            {email: 'user@crate.com'}
+          ]
+        }
+      })
+      done();
+  })
+})
+
