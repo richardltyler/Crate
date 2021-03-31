623d3380052f2970c6e278b72e1d5a84ebf4b124
Allison Dietz <dietz.allison@outlook.com>
2021-03-30 19:58:39 -0600

    Build initial structure of Form component

--- code/web/src/modules/survey/Form.js
+++ code/web/src/modules/survey/Form.js
@@ -6,4 +6,37 @@ import PropTypes from 'prop-types'
 import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
 import { grey, grey2 } from '../../ui/common/colors'
+import Button from '../../ui/button'
+import ImageTile from '../../ui/image/Tile'
 
+const Form = (props) => {
+
+  const [stateSample, setStateSample] = useState({})
+
+  return (
+
+    <>
+    <H4 >{ props.data.categoryTitle }</H4>
+    <p style={{ marginTop: '1em', color: grey2 }}>Please select at least one option, then click `NEXT` to continue...</p>
+    
+    <Button type="button" theme="secondary" disabled={false}>
+    Next ➤
+    </Button>
+
+    <Grid>
+      <GridCell justifyCenter={true}>
+        <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
+      </GridCell>
+      <GridCell justifyCenter={true}>
+        <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
+      </GridCell>
+      <GridCell justifyCenter={true}>
+        <ImageTile width={170} height={250} shadow={level1} image={`${ APP_URL }/images/stock/men/2.jpg`}/>
+      </GridCell>
+    </Grid>
+    </>
+
+  )
+}
+
+export default Form;
